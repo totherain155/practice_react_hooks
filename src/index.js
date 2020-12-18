d6baf575c7443b6c8ce88f10bb5a50e415c80813
totherain155 <totherain155@gmail.com>
2020-12-19 08:31:35 +0900

    useFadeIn

--- src/index.js
+++ src/index.js
@@ -1,37 +1,44 @@
 import React, { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { unstable_concurrentAct } from "react-dom/test-utils";
 //useFadeIn , useEffect , useRef사용 
-
 
 const useFadeIn = (duration = 1, delay = 0) => {
 
     const element = useRef()
 
     useEffect(() => {
-
         if (element.current) {
-            const { current } = element;
-            current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`;
-            current.style.opacity = 1;
-        }
+            const { current } = element
+            current.style.transition = `opacity ${duration}s ease-in-out ${delay}s`
+            current.style.opacity = 1
 
+        }
     }, [])
 
 
-    return { ref: element, style: { opacity: 0 } }
+    return { ref: element, style: { opacity: 0 } };
+
+
 }
+
 
 
 const App = () => {
-    const fadeInH2 = useFadeIn(1, 2)
-    const fadeInP = useFadeIn(5, 10)
+
+    const fadeInH2 = useFadeIn(3, 5)
+
+    const fadeInP = useFadeIn(6, 10)
+
+
     return (
         <div>
             <h2 {...fadeInH2}>Fade In</h2>
-            <p {...fadeInP}>we are loading </p>
-        </div >
+            <p  {...fadeInP}>we are comming</p>
+        </div>
     )
 }
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
