3bae857c6fb8fbcc35b7540a320e0299aa640d51
totherain155 <totherain155@gmail.com>
2021-01-02 11:58:27 +0900

    useEffect

---
+++ src/practice_useEffect/index.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    const sayHello = () => {
+        console.log("hi");
+    };
+
+    const [number, setNumber] = useState(0);
+    const [aNumber, setAnumber] = useState(0);
+    useEffect(sayHello, [aNumber]);
+
+    return (
+        <div>
+            <button onClick={() => setNumber(number + 1)}>{number} </button>
+            <button onClick={() => setAnumber(aNumber + 1)}>{aNumber} </button>
+        </div>
+    );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    rootElement
+);
