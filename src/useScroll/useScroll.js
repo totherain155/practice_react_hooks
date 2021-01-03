3f97e6eaceb6e17fa8f493b48b2e4212f1773910
totherain155 <totherain155@gmail.com>
2021-01-03 19:26:33 +0900

    useScroll

--- src/useScroll/useScroll.js
+++ src/useScroll/useScroll.js
@@ -1,36 +1,34 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
-// useScroll function 만들어서 useEffect, useState 사용하기
-
 const useScroll = () => {
     const [state, setState] = useState({
         x: 0,
         y: 0
-    })
+    });
     const handleScroll = () => {
-        setState({ x: window.scrollX, y: window.scrollY })
-    }
-
+        setState({ y: window.scrollY, x: window.scrollX });
+    };
     useEffect(() => {
-        window.addEventListener("scroll", handleScroll)
+        window.addEventListener("scroll", handleScroll);
         return () => {
-            window.removeEventListener("scroll", handleScroll)
-        }
-    }, [])
-    return state;
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
-}
+    return state;
+};
 
 const App = () => {
-    const { y } = useScroll()
+    const { y } = useScroll();
     return (
-        <div style={{ height: "1000vh" }}>
-
-            <h2 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>Hi</h2>
-        </div >
-    )
-}
+        <div className="App" style={{ height: "1000vh" }}>
+            <h2 style={{ position: "fixed", color: y > 100 ? "red" : "blue" }}>
+                Hi
+            </h2>
+        </div>
+    );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
